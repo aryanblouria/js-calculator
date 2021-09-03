8914f1de13ae4ba90f1e20a452aebca3a20b2f6e
Aryan Blouria <57865255+aryanblouria@users.noreply.github.com>
2021-09-03 17:31:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,44 @@ const screen = document.querySelector(".calculator-screen");
 let expression, result;
 let start = true;
 
-function Calc(n) {
-  if((n>=0 && n<=9) || n=='.') {
-    if(start) {
+function Calc(n) 
+{
+  if((n>=0 && n<=9) || n=='.') 
+  {
+    if(start) 
+    {
       screen.value += n;
-    } else {
+    } 
+    else 
+    {
       screen.value += n;
       start = true;
     }
     expression = screen.value;
-  } else if (n == '+' || n == '-' || n == '×' || n == '÷' || n == "%") {
+  }
+  else if (n == '+' || n == '-' || n == '×' || n == '÷' || n == "%") 
+  {
     if (n == '×') {n = '*'}
     if (n == '÷') {n = '/'}
+    
     expression = screen.value + " " + n + " ";
     screen.value = expression;
     start = false;
-  } else if (n == '=') {
+  } 
+  else if (n == '=') 
+  {
     result = eval(expression);
     screen.value = result;
     expression = "";
     result = "";
-  } else if (n == 'C') {
+  } 
+  else if (n == 'C') 
+  {
     screen.value = "";
     expression = "";
-  } else if (n == 'backspace') {
+  } 
+  else if (n == 'backspace') 
+  {
     let str = screen.value;
     screen.value = str.substr(0, str.length - 1)
   }
